f848d72b19f782a673ec48841e12a500fa759af2
zerlok <danil.troshnev@gmail.com>
2025-11-25 20:10:55 +0100

    patch pytest fixture to fix pytest run via nox

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import importlib
+import shutil
 import typing as t
+from pathlib import Path
 
 import pytest
 from _pytest.fixtures import SubRequest
@@ -34,6 +36,11 @@ def async_mode_ini_options() -> t.Optional[str]:
 
 @pytest.fixture
 def pytester(pytester: Pytester, async_mode_ini_options: t.Optional[str]) -> Pytester:
+    # NOTE: fixes run via nox
+    sandbox_path = pytester.path / "tests"
+    sandbox_path.mkdir(parents=True, exist_ok=True)
+    shutil.copytree(Path(__file__).parent / "stub", sandbox_path / "stub")
+
     if async_mode_ini_options is not None:
         pytester.makeini(f"""
 [tool.pytest.ini_options]
